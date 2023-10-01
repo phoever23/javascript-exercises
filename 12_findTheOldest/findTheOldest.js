f6b1da7e4d5ad7b22f3d5a1a7328ab010bcd0689
TAKA <gaoming93@foxmail.com>
2023-10-01 20:52:22 +0800

    finish 12 js exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.map(person => {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    })
+    return people.sort((a,b) => (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth))[0];
 };
 
 // Do not edit below this line
