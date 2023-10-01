8be743180a4f6ceee536e5f73b11026ebbc0a082
TAKA <gaoming93@foxmail.com>
2023-10-01 19:59:20 +0800

    finish 09 javascript exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    return input.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase().split("").filter(item => item !== " ").join("")
+    === input.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase().split("").filter(item => item !== " ").reverse().join("");
 };
 
 // Do not edit below this line
