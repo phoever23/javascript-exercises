1e125e26e40af7b51c64a2d016a2db3b12df2e35
TAKA <gaoming93@foxmail.com>
2023-10-01 19:35:14 +0800

    finish 08 calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    let factorial = 1;
+    let i = 1;
+    while (i <= n) {
+      factorial *= i;
+      i ++;
+    }
+    return factorial;
+  }
 };
 
 // Do not edit below this line
